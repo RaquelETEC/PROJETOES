d81289c9342bed8041a562ddbdd449aaee79dfaf
Raquel Gonçalves <53949793+RaquelETEC@users.noreply.github.com>
2022-03-29 22:27:10 -0300

    Feito a tela principal com os botões do menu

---
+++ CadUsuarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teladelogin
+{
+    public partial class CadUsuarios : Form
+    {
+        public CadUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void CadUsuarios_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Conexao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teladelogin
+{
+    public class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+        //construtor 
+        public Conexao()
+        {
+            con.ConnectionString = "";
+        }
+        //metodo  conectar 
+      //  public SqlConnection conectar()
+      //  {
+ //           return;
+
+   //     }
+        //metodo desconectar 
+        public void desconectar()
+        {
+
+        }
+    }
+}
