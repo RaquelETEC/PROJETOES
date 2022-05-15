2d6f311f9dac07d27ae9e27f88699dd0b07ca08c
Raquel Gonçalves <53949793+RaquelETEC@users.noreply.github.com>
2022-05-15 18:59:45 -0300

    Add files via upload

---
+++ sistemaclube/sistemaclube/frlogin.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace sistemaclube
+{
+    partial class frlogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frlogin));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2_SENHA = new System.Windows.Forms.TextBox();
+            this.textBox1_LOGIN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1_logar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-8, -9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(596, 539);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(633, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(343, 26);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "SISTEMA GERENCIADOR DE QUADRAS";
+            // 
+            // textBox2_SENHA
+            // 
+            this.textBox2_SENHA.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2_SENHA.Location = new System.Drawing.Point(727, 297);
+            this.textBox2_SENHA.Name = "textBox2_SENHA";
+            this.textBox2_SENHA.Size = new System.Drawing.Size(237, 31);
+            this.textBox2_SENHA.TabIndex = 10;
+            // 
+            // textBox1_LOGIN
+            // 
+            this.textBox1_LOGIN.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_LOGIN.Location = new System.Drawing.Point(727, 227);
+            this.textBox1_LOGIN.Name = "textBox1_LOGIN";
+            this.textBox1_LOGIN.Size = new System.Drawing.Size(237, 31);
+            this.textBox1_LOGIN.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(633, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "SENHA";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(633, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 26);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "E-MAIL";
+            // 
+            // button1_logar
+            // 
+            this.button1_logar.Location = new System.Drawing.Point(727, 388);
+            this.button1_logar.Name = "button1_logar";
+            this.button1_logar.Size = new System.Drawing.Size(186, 69);
+            this.button1_logar.TabIndex = 12;
+            this.button1_logar.Text = "LOGAR";
+            this.button1_logar.UseVisualStyleBackColor = true;
+            this.button1_logar.Click += new System.EventHandler(this.button1_logar_Click);
+            // 
+            // frlogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 519);
+            this.Controls.Add(this.button1_logar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2_SENHA);
+            this.Controls.Add(this.textBox1_LOGIN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frlogin";
+            this.Text = "SISTEMA DE QUADRAS";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2_SENHA;
+        private System.Windows.Forms.TextBox textBox1_LOGIN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1_logar;
+    }
+}
